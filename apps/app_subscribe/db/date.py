94820000cf4c14d5bb4134f2346b7c1c89021f31
sterenin <tortvikk@gmail.com>
2024-10-23 15:07:30 +0300

    подсчет времени

--- apps/app_subscribe/db/date.py
+++ apps/app_subscribe/db/date.py
@@ -1,19 +1,24 @@
 from datetime import timedelta, datetime
 
-from django.db.models import QuerySet, F
+from django.db.models import QuerySet, F, Func, DateTimeField
 from django.contrib.auth.models import AbstractBaseUser
 
 
 from apps.app_subscribe.models import UserSubscribe
 
 
+class IntervalSeconds(Func):
+    function = 'INTERVAL'
+    template = "(%(expressions)s * %(function)s '1 days')"
+
+
 def get_leftover_days(queryset: QuerySet[UserSubscribe], user: AbstractBaseUser) -> timedelta:
     today = datetime.now()
     result = queryset.filter(
-            user__id__in = user.id
+            user = user
         ).annotate(
-            total = F('subscribe__day_range') + F('created_at') - today
-        ).values(
-            'total'
+            total = F('created_at') - today
         ).last()
-    return result['total']
+    if result is None:
+        return None
+    return result.total
