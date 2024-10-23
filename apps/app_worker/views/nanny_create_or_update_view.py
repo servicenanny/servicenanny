89788a62375f3a44723ced1964cede7a0d1420c2
sterenin <tortvikk@gmail.com>
2024-10-23 20:49:01 +0300

    nannycreateorupdate

--- apps/app_worker/views/nanny_create_or_update_view.py
+++ apps/app_worker/views/nanny_create_or_update_view.py
@@ -1,4 +1,6 @@
+from dataclasses import dataclass
 from typing import Any
+from django.forms import BaseModelForm
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import CreateView
@@ -8,6 +10,18 @@ from apps.app_worker.models import Nanny
 from apps.app_worker.forms import NannyForm, UserForm
 
 
+@dataclass
+class MultipleNannyTypeForm:
+    nanny_type: type[NannyForm]
+    user_type: type[UserForm]
+
+
+@dataclass
+class MultipleNannyForm:
+    nanny_form: NannyForm
+    user_form: UserForm
+
+
 class NannyCreateOrUpdateView(CreateView):
     template_name = 'nanny_register.html'
     
@@ -15,27 +29,31 @@ class NannyCreateOrUpdateView(CreateView):
         super().__init__(**kwargs)
         self.user = None
     
-    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
-        if request.method == 'POST':
-            user_form = UserForm(request.POST)
-            nanny_form = NannyForm(request.POST)
-            if user_form.is_valid() and nanny_form.is_valid():
-                user_form.save()
-                nanny_form.save()
-                return HttpResponseRedirect('/home')        
-            else:
-                context = {
-                    'user_form': user_form,
-                    'nanny_form': nanny_form,
-                }
-        else:
-            context = {
-                'user_form': UserForm(),
-                'nanny_form': NannyForm(),
-            }
-        return render(request, context)
+    # def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+    #     user_form = UserForm(request.POST)
+    #     nanny_form = NannyForm(request.POST)
+    #     if user_form.is_valid() and nanny_form.is_valid():
+    #         user_form.save()
+    #         nanny_form.save()
+    #         return HttpResponseRedirect('/home')        
+    #     else:
+    #         context = {
+    #             'user_form': user_form,
+    #             'nanny_form': nanny_form,
+    #         }
+    #     return render(request, 'nanny_register.html', context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        ...
-        return context
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        return super().post(request, *args, **kwargs)
+    
+    def get_form_class(self):
+        return MultipleNannyTypeForm(NannyForm, UserForm)
+    
+    def get_form(self, form_class: MultipleNannyTypeForm = None) -> BaseModelForm:
+        """Return an instance of the form to be used in this view."""
+        if form_class is None:
+            form_class = self.get_form_class()
+        return MultipleNannyForm(
+            nanny_form = form_class.nanny_type(**self.get_form_kwargs()),
+            user_form = form_class.user_type(**self.get_form_kwargs())
+        )
