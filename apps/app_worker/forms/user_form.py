ddc4934abb3fa86de76e8dc0ebc9ee9f8bb1e702
sterenin <tortvikk@gmail.com>
2024-10-23 18:02:30 +0300

    user_ и nanny_form done

---
+++ apps/app_worker/forms/user_form.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from django.contrib.auth import get_user_model
+
+
+class UserForm(ModelForm):   
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name']
