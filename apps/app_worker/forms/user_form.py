e30a705f979abaefc277c2fa19be34168c361c0b
Semyon <115490371+siskamyon@users.noreply.github.com>
2024-10-25 11:58:33 +0300

    Revert "Dev semen"

--- apps/app_worker/forms/user_form.py
+++ apps/app_worker/forms/user_form.py
@@ -6,9 +6,6 @@ class UserForm(ModelForm):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = get_user_model()
