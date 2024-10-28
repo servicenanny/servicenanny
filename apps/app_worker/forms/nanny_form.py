ec7608e8228de00413dde0883bc032070ec89f70
Ринат Кучаев <RZKuchaev@stud.kpfu.ru>
2024-10-28 07:02:43 +0300

    1. Update nanny_register form
    2. Change header_yellow

--- apps/app_worker/forms/nanny_form.py
+++ apps/app_worker/forms/nanny_form.py
@@ -8,6 +8,11 @@ class NannyForm(ModelForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
+        self.fields['phone_number'].widget.attrs['placeholder'] = "+7 999 999 99 99"
+        self.fields['cost_per_hour'].widget.attrs['placeholder'] = "от 450₽"
+        self.fields['experience'].widget.attrs['placeholder'] = "10 лет"
+        self.fields['describe'].widget.attrs['placeholder'] = "Работаю с детьми от 3-х лет..."
+
 
     class Meta:
         model = Nanny
