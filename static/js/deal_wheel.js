330e4dc411bd416450d540915e892bc1fdbb2a76
Ринат Кучаев <RZKuchaev@stud.kpfu.ru>
2024-12-26 17:56:00 +0300

    New version

--- static/js/deal_wheel.js
+++ static/js/deal_wheel.js
@@ -1,24 +1,31 @@
-// создаём переменные для быстрого доступа ко всем объектам на странице — блоку в целом, колесу, кнопке и язычку
 const wheel = document.querySelector("#deal-wheel");
-const spinner = wheel.querySelector(".spinner");
-const trigger = wheel.querySelector(".btn-spin");
+let spinner = wheel.querySelector(".spinner");
+const triggers = wheel.querySelectorAll(".btn-spin");
 const ticker = wheel.querySelector(".ticker");
 const prizeNodes = wheel.querySelectorAll(".prize");
 
+const context_nanny = wheel.querySelector('#spinner-nanny')
+const spinner_nanny = context_nanny.querySelector(".spinner");
+const trigger_nanny = context_nanny.querySelectorAll(".btn-spin");
+const ticker_nanny = context_nanny.querySelector(".ticker");
+const prizeNodes_nanny = context_nanny.querySelectorAll(".prize");
 
-// переменная для анимации⠀
+const context_parent = wheel.querySelector('#spinner-parent')
+const spinner_parent = context_nanny.querySelector(".spinner");
+const trigger_parent = context_nanny.querySelectorAll(".btn-spin");
+const ticker_parent = context_nanny.querySelector(".ticker");
+const prizeNodes_parent = context_nanny.querySelectorAll(".prize");
+
 let tickerAnim;
-// угол вращения
 let rotation = 0;
-// текущий сектор⠀
 let currentSlice = 0;
 
-// прописываем CSS-классы, которые будем добавлять и убирать из стилей
 const spinClass = "is-spinning";
 const selectedClass = "selected";
-// получаем все значения параметров стилей у секторов
-const spinnerStyles = window.getComputedStyle(spinner);
 
+let spinnerStyles;
+const numSlices = prizeNodes.length;
+const prizeSlice = 360 / numSlices;
 
 const spinertia = (min, max) => {
     min = Math.ceil(min);
@@ -26,51 +33,113 @@ const spinertia = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-// функция запуска вращения с плавной остановкой
 const runTickerAnimation = () => {
     const values = spinnerStyles.transform.split("(")[1].split(")")[0].split(",");
     const a = values[0];
     const b = values[1];  
     let rad = Math.atan2(b, a);
-    
     if (rad < 0) rad += (2 * Math.PI);
     tickerAnim = requestAnimationFrame(runTickerAnimation);
 };
 
 const selectPrize = () => {
-    const selected = Math.floor(rotation / prizeSlice);
+    const selected = Math.floor(rotation / prizeSlice) % numSlices;
     prizeNodes[selected].classList.add(selectedClass);
 };
 
+const setPrize = () => {
+    if(wheel.querySelector('#spinner-nanny-tab').getAttribute('aria-selected') === 'true') {
+        const context = wheel.querySelector('#spinner-nanny')
+        const btn = context.querySelector('.btn-spin')
+        const promoBlock = context.querySelector(".win");
+        promoBlock.removeAttribute('hidden')
+        promoBlock.addEventListener("click", () => {
+        const textToCopy = "NANNY";
+        navigator.clipboard.writeText(textToCopy)
+            .then(() => {
+                alert("Промокод скопирован: " + textToCopy);
+            })
+            .catch((err) => {
+                console.error("Не удалось скопировать текст: ", err);
+            });
+        });
+        btn.remove()
+    }
+    else {
+        const context = wheel.querySelector('#spinner-parent')
+        const promoBlock = context.querySelector(".win");
+        const btn = context.querySelector('.btn-spin')
+        promoBlock.removeAttribute('hidden')
+        promoBlock.addEventListener("click", () => {
+        const textToCopy = "PARENT";
+        navigator.clipboard.writeText(textToCopy)
+            .then(() => {
+                alert("Промокод скопирован: " + textToCopy);
+            })
+            .catch((err) => {
+                console.error("Не удалось скопировать текст: ", err);
+            });
+        });
+        btn.remove()
+    }
+    
+    
+}
 
-trigger.addEventListener("click", () => {
-    // делаем её недоступной для нажатия
-    trigger.disabled = true;
-    // задаём начальное вращение колеса
-    rotation = Math.floor(Math.random() * 360 + spinertia(2000, 5000));
-    // добавляем колесу класс is-spinning, с помощью которого реализуем нужную отрисовку
-    wheel.classList.add(spinClass);
-    // через CSS говорим секторам, как им повернуться
+const stopAtPrize = (prizeIndex) => {
+    const targetRotation = prizeIndex * prizeSlice + (360 * Math.round(Math.random() * 6)); // 5 полных оборотов для эффекта
+    rotation = targetRotation;
     spinner.style.setProperty("--rotate", rotation);
-    // возвращаем язычок в горизонтальную позицию
+};
+
+
+trigger_nanny.addEventListener("click", () => {
+    trigger_nanny.disabled = true;
+    spinnerStyles = window.getComputedStyle(spinner_nanny);
+    wheel.classList.add(spinClass);
     ticker.style.animation = "none";
-    // запускаем анимацию вращение
+    const desiredPrizeIndex = 4;
+    stopAtPrize(desiredPrizeIndex);
     runTickerAnimation();
 });
 
 
-// отслеживаем, когда закончилась анимация вращения колеса
+trigger_parent.addEventListener("click", () => {
+    trigger_nanny.disabled = true;
+    spinnerStyles = window.getComputedStyle(spinner_parent);
+    wheel.classList.add(spinClass);
+    ticker.style.animation = "none";
+    const desiredPrizeIndex = 4;
+    stopAtPrize(desiredPrizeIndex);
+    runTickerAnimation();
+});
+
+
+triggers.forEach((trigger) => {
+    trigger.addEventListener("click", () => {
+        if(wheel.querySelector('#spinner-nanny-tab').getAttribute('aria-selected') === 'true') {
+            spinner = wheel.querySelector('#spinner-nanny').querySelector('.spinner');
+            spinnerStyles = window.getComputedStyle(spinner);
+        }
+        else {
+            spinner = wheel.querySelector('#spinner-parent').querySelector('.spinner');
+            spinnerStyles = window.getComputedStyle(spinner);
+        }
+        trigger.disabled = true;
+        wheel.classList.add(spinClass);
+        ticker.style.animation = "none";
+        const desiredPrizeIndex = 4;
+        stopAtPrize(desiredPrizeIndex);
+        runTickerAnimation();
+    });
+})
+
+
 spinner.addEventListener("transitionend", () => {
-    // останавливаем отрисовку вращения
     cancelAnimationFrame(tickerAnim);
-    // получаем текущее значение поворота колеса
     rotation %= 360;
-    // выбираем приз
     selectPrize();
-    // убираем класс, который отвечает за вращение
     wheel.classList.remove(spinClass);
-    // отправляем в CSS новое положение поворота колеса
     spinner.style.setProperty("--rotate", rotation);
-    // делаем кнопку снова активной
-    trigger.disabled = false;
+    setPrize()
 });
